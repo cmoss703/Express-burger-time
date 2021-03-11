392708e5d1c6ef3dfcfbf6fa9b95857a4b8a6588
Christina <christinalmoss@gmail.com>
2021-03-10 19:19:34 -0600

    Started connections and server file

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require('express');
 
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static('app/public'));
+
+// Routes
+// =============================================================
+require('./app/routes/api-routes.js')(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
